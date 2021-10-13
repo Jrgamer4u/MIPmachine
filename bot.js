c12a79d23df06eb62833085d71014c459ff9063f
Jrgamer4trash <66277204+Jrgamer4u@users.noreply.github.com>
2021-10-13 00:55:11 +0000

    removed "connected" and used newest version of scratch for discord.

--- bot.js
+++ bot.js
@@ -1,42 +1,26 @@
-const keepAlive = require("./server.js")
-                let Discord;
-                let Database;
-                if(typeof window !== "undefined"){
-                    Discord = DiscordJS;
-                    Database = EasyDatabase;
-                } else {
-                    Discord = require("discord.js");
-                    Database = require("easy-json-database");
-                }
-                const delay = (ms) => new Promise((resolve) => setTimeout(() => resolve(), ms));
+                (async()=>{
+                const keepAlive = require("./server.js")
+                const Discord = require("discord.js");
+                const Database = require("easy-json-database");
+                const devMode = typeof __E_IS_DEV !== "undefined" && __E_IS_DEV;
+                const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
                 const s4d = {
                     Discord,
-                    client: null,
-                    tokenInvalid: false,
-                    reply: null,
-                    joiningMember: null,
-                    database: new Database("./db.json"),
+                    database: new Database(`${devMode ? S4D_NATIVE_GET_PATH : "."}/db.json`),
+                    joiningMember:null,
+                    reply:null,
+                    tokenInvalid:false,
+                    tokenError: null,
                     checkMessageExists() {
                         if (!s4d.client) throw new Error('You cannot perform message operations without a Discord.js client')
                         if (!s4d.client.readyTimestamp) throw new Error('You cannot perform message operations while the bot is not connected to the Discord API')
                     }
                 };
                 s4d.client = new s4d.Discord.Client({
-                    fetchAllMembers: true
+                    intents: [Object.values(s4d.Discord.Intents.FLAGS).reduce((acc, p) => acc | p, 0)],
+                    partials: ["REACTION"]
                 });
-                s4d.client.on('raw', async (packet) => {
-                    if(['MESSAGE_REACTION_ADD', 'MESSAGE_REACTION_REMOVE'].includes(packet.t)){
-                        const guild = s4d.client.guilds.cache.get(packet.d.guild_id);
-                        if(!guild) return;
-                        const member = guild.members.cache.get(packet.d.user_id) || guild.members.fetch(d.user_id).catch(() => {});
-                        if(!member) return;
-                        const channel = s4d.client.channels.cache.get(packet.d.channel_id);
-                        if(!channel) return;
-                        const message = channel.messages.cache.get(packet.d.message_id) || await channel.messages.fetch(packet.d.message_id).catch(() => {});
-                        if(!message) return;
-                        s4d.client.emit(packet.t, guild, channel, message, member, packet.d.emoji.name);
-                    }
-                });
+
                 function colourRandom() {
   var num = Math.floor(Math.random() * Math.pow(2, 24));
   return '#' + ('00000' + num.toString(16)).substr(-6);
@@ -70,18 +54,13 @@ function mathRandomInt(a, b) {
 }
 
 
-s4d.client.login(process.env['TOKEN']).catch((e) => { s4d.tokenInvalid = true; s4d.tokenError = e; });
+await s4d.client.login(process.env['TOKEN']).catch((e) => { s4d.tokenInvalid = true; s4d.tokenError = e; });
 
 keepAlive()
 
-s4d.client.on('ready', async () => {
-  s4d.client.channels.cache.get('870522417079017474').send(String('connected'));
-
-});
-
-s4d.client.on('message', async (s4dmessage) => {
+s4d.client.on('messageCreate', async (s4dmessage) => {
   if ((s4dmessage.channel) != s4d.client.channels.cache.get(process.env['SpyEye'])) {
-    s4d.client.channels.cache.get(process.env['SpyEye']).send(String(([s4dmessage.content].join(''))));
+    s4d.client.channels.cache.get(process.env['SpyEye']).send(String((s4dmessage.content)));
   }
   if ((s4dmessage.content) == 'hi') {
     s4dmessage.channel.send(String('nah'));
@@ -111,7 +90,7 @@ s4d.client.on('message', async (s4dmessage) => {
   } else if ((s4dmessage.content) == 'Total clicks') {
     s4dmessage.channel.send(s4d.database.get(String('Total Clicks')));
   } else if ((s4dmessage.content) == 'Version') {
-    s4dmessage.channel.send(String('Version 1.1.0.6 + SpyEye 1.0.0.2'));
+    s4dmessage.channel.send(String('Version 1.1.0.3 + SpyEye 1.0'));
   } else if (String((s4dmessage.content)).includes(String('sus'))) {
     s4dmessage.channel.send(String('a m o n g u s'));
   } else if ((s4dmessage.content) == 'What\'s the Time?') {
@@ -132,7 +111,7 @@ s4d.client.on('message', async (s4dmessage) => {
       s4d.client.channels.cache.get('871495748829728838').send(String((String((s4dmessage.content)).replace(new RegExp(String('OBR Suggestion'), 'g'), String('Suggestion')))));
     }
   } else if ((s4dmessage.content) == 'SimCity') {
-    (s4dmessage.channel).send(String('https://gamepleton.com/wp-content/uploads/2020/08/simcity3000-railways-cbd@2x.png'));
+    (s4dmessage.channel).send(String('outlier'));
     (s4dmessage.channel).awaitMessages((m) => m.author.id === (s4dmessage.member).id, { time: (0.016*60*1000), max: 1 }).then(async (collected) => { s4d.reply = collected.first().content;
        s4dmessage.channel.send(String('GodSmack'));
 
@@ -141,10 +120,13 @@ s4d.client.on('message', async (s4dmessage) => {
   if ((s4dmessage.content) == 'Help, OBR!') {
     s4dmessage.channel.send(String((['Command List','\n','Commands created in 7/30/2021','\n','hi','\n','Help, OBR!','\n',(s4dmessage.member || await s4dmessage.guild.members.fetch(s4dmessage.author.id)).nickname,'\n','pi','\n','I\'m not racist but','\n','color','\n','among us','\n','OBR'].join(''))));
     s4dmessage.channel.send(String((['Command List','\n','Commands created in 7/31/2021','\n','Click','\n','My clicks','\n','Total clicks','\n','sus','\n','Version','\n','What\'s the Time?','\n','Click Soft Risk','\n','Click Hard Risk'].join(''))));
+    s4dmessage.channel.send(String((['Command List','\n','Command created in 8/28/2021','\n','SimCity'].join(''))));
     s4dmessage.channel.send(String('To Send Suggestions, Just Say <OBR Suggestion> at the beginning then say what you suggest.'));
   }
 
 });
 
-                s4d;
+
+                return s4d;
+                })();
             
