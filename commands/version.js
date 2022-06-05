bccaf2a527e125de94d70e659153c0dd068128fb
Jrgamer4u <66277204+Jrgamer4u@users.noreply.github.com>
2022-06-05 23:53:37 +0000

    fix click

--- commands/version.js
+++ commands/version.js
@@ -5,6 +5,6 @@ module.exports = {
 		.setName('version')
 		.setDescription('shows version'),
 	async execute(interaction) {
-		return interaction.reply('MipMachine EX 1.1.2 | Base: OBR 1.4.2');
+		return interaction.reply('MipMachine EX 1.1.3 | Base: OBR 1.4.3');
 	},
 };
