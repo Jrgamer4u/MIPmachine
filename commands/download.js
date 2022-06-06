813aba72d14d8b79f740189b96508d81891721ef
Jrgamer4u <66277204+Jrgamer4u@users.noreply.github.com>
2022-06-06 18:45:13 +0000

    fix mistake

--- commands/download.js
+++ commands/download.js
@@ -1,6 +1,6 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const { MessageAttachment } = require('discord.js');
-const file = new MessageAttachment('images/balls.png');
+const file = new MessageAttachment('images/DOWNLOAD.png');
 
 module.exports = {
 	data: new SlashCommandBuilder()
