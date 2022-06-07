2184354f03f8df5b5dada6b6e734fafa21f713c4
Jrgamer4u <66277204+Jrgamer4u@users.noreply.github.com>
2022-06-06 20:51:52 -0700

    fs

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,9 +1,9 @@
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
-const fs = require('fs');
+const { readdirSync } = require('fs');
 
 const commands = [];
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+const commandFiles = readdirSync('./commands').filter(file => file.endsWith('.js'));
 
 for (const file of commandFiles) {
 	const command = require(`./commands/${file}`);
--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const keepAlive = require("./server.js");
-const fs = require('fs');
+const { readdirSync } = require('fs');
 const { Client, Collection, Intents } = require('discord.js');
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
 client.commands = new Collection();
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+const commandFiles = readdirSync('./commands').filter(file => file.endsWith('.js'));
 
 for (const file of commandFiles) {
 	const command = require(`./commands/${file}`);
