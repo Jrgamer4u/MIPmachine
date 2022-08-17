45b21a8837b7e44cfdf3df056a335bee18eb0904
Jrgamer4u <66277204+Jrgamer4u@users.noreply.github.com>
2022-08-16 23:27:48 -0700

    rewrite

--- server.js
+++ server.js
@@ -1,21 +1,11 @@
 const express = require('express');
 const server = express();
 
-server.all('/', (req, res)=>{
+server.get('/', function (req, res) {
   res.send('Your bot is alive!')
 })
 function keepAlive(){
   server.listen(3000, ()=>{console.log("Server is Ready!")});
 }
-
-function resolved(result) {
-  console.log('Resolved');
-}
-
-function rejected(result) {
-  console.error(result);
-}
-
-Promise.reject(new Error()).then(resolved, rejected);
 
 module.exports = keepAlive;
