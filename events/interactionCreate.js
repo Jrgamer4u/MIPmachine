e906d2589dd66cb98cd09a060b556918c6ad1ab7
Jrgamer4u <66277204+Jrgamer4u@users.noreply.github.com>
2022-08-26 06:39:46 +0000

    clean up interactionCreate and index

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,18 +1,4 @@
-const fs = require('node:fs');
-const path = require('node:path');
-const { Client, Collection, InteractionType, GatewayIntentBits } = require('discord.js');
-
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
-
-client.commands = new Collection();
-const commandsPath = path.join(__dirname, '../commands');
-const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
-
-for (const file of commandFiles) {
-	const filePath = path.join(commandsPath, file);
-	const command = require(filePath);
-	client.commands.set(command.data.name, command);
-}
+const { InteractionType } = require('discord.js');
 
 module.exports = {
 	name: 'interactionCreate',
