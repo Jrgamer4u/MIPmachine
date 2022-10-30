f2a443dbeb54983584d7b5deec45d45f4bf21d74
Jrgamer4u <66277204+Jrgamer4u@users.noreply.github.com>
2022-10-30 12:31:46 -0700

    whar

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const keepAlive = require("./server.js");
 const fs = require('node:fs');
 const path = require('node:path');
-const { Client, Collection, GatewayIntentBits } = require('discord.js');
+const { Client, Collection, GatewayIntentBits, REST, Routes } = require('discord.js');
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
+const commands = [];
 client.commands = new Collection();
 const commandsPath = path.join(__dirname, 'commands');
 const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
@@ -12,8 +13,15 @@ const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('
 for (const file of commandFiles) {
 	const filePath = path.join(commandsPath, file);
 	const command = require(filePath);
+	commands.push(command.data.toJSON());
 	client.commands.set(command.data.name, command);
 }
+
+const rest = new REST({ version: '10' }).setToken(process.env.token);
+
+rest.put(Routes.applicationCommands(process.env.clientId), { body: commands })
+	.then(() => console.log('Successfully registered application commands.'))
+	.catch(console.error);
 
 const eventsPath = path.join(__dirname, 'events');
 const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
