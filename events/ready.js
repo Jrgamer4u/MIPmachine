342314218614ec3142e28ee59596da9632ef87d7
Jrgamer4u <66277204+Jrgamer4u@users.noreply.github.com>
2022-12-06 19:54:29 -0800

    remove message

--- events/ready.js
+++ events/ready.js
@@ -2,7 +2,6 @@ module.exports = {
 	name: 'ready',
 	once: true,
 	execute(client) {
-		client.channels.cache.get(process.env.connected).send('Connected');
-		client.user.setActivity('MipMachine EX | Slash Commands!');
+		client.user.setActivity('MM EX | Slash Commands!');
 	},
 };
